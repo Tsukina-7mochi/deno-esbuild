49ab9eff35e0831088532dc5fecf71453fdca485
Tsukina-7mochi <tsukina7mochi@gmail.com>
2022-11-22 19:49:26 +0900

    add react

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'https://esm.sh/react@18';
+import ReactDOM from 'https://esm.sh/react-dom@18';
+
+const App = () => (
+    <div>
+        <h2>This is a React app!</h2>
+    </div>
+);
+
+export default App;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import './someModule.ts';
 import './style.scss';
+import App from './App.tsx';
+import ReactDOM from 'https://esm.sh/react-dom@18';
 
 globalThis.addEventListener('click', () => {
   document.querySelector('button')?.addEventListener('click', () => {
-    window.alert("Hello esbuild!")
+    window.alert('Hello esbuild!')
   });
+
+  const reactRoot = document.getElementById('reactRoot');
+  console.log(reactRoot);
+  if(!reactRoot) {
+    throw Error('Failed to get #reactRoot');
+  }
+
+  ReactDOM.render(App(), reactRoot);
 });
