95664b947a0275868f5647a548a5ad45a6360352
Tsukina-7mochi <tsukina7mochi@gmail.com>
2022-11-24 10:07:21 +0900

    rename import map

--- esbuild.common.ts
+++ esbuild.common.ts
@@ -2,7 +2,7 @@ import * as esbuild from 'https://deno.land/x/esbuild@v0.15.10/mod.js';
 import { posix } from 'https://deno.land/std/path/mod.ts';
 import { sassPlugin } from 'npm:esbuild-sass-plugin';
 import { cache } from 'https://deno.land/x/esbuild_plugin_cache/mod.ts';
-import importmap from './import_map.json' assert {type: 'json'};
+import importmap from './browser_import_map.json' assert {type: 'json'};
 
 const dirName = new URL('.', import.meta.url).pathname;
 const srcPath = posix.join(dirName, 'src');
